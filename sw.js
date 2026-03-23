37dcbec988c21d4b1354b0588c17744427c1a007
crewlogistics <brett@crewlogistics.com>
2026-03-23 12:48:53 +0700

    v1.7.1: Cache busting - force refresh on all devices

--- sw.js
+++ sw.js
@@ -1,7 +1,11 @@
-const CACHE = 'gocrew-v1';
+const CACHE = 'gocrew-v1.7';
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/gocrew-checkin-demo/', '/gocrew-checkin-demo/index.html'])));
 });
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
